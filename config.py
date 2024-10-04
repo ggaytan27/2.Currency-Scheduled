fb26580206a03d3693af4fcd44c22f1139bb09a4
Gilberto Gaytan <gilberto.gaytanpadilla@us.bosch.com>
2024-10-04 15:31:42 -0600

    trabajando en lap personal

---
+++ config.py
@@ -0,0 +1,12 @@
+PROXI_USER = 'US\\GGI2JU'
+PROXI_PWD = 'BoschGil041189+'
+EMAIL = 'gilberto.gaytanpadilla@us.bosch.com'
+SUBJECT = 'Tipo de Cambio'
+PROXIES = {
+    'http': 'http://rb-proxy-na.bosch.com:8080',
+    'https': 'http://rb-proxy-na.bosch.com:8080'
+}
+HEADERS = {
+    'Bmx-Token': '80f8b40305f8c5067445bd8868c2be2fd29f526c597fbadffa7cca63509a25a7',  # Cambia esto por el header que necesites
+    'Content-Type': 'application/json',
+}
