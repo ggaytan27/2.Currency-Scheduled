3870e898cbd0225ab102062e4b2089c42599a12d
Beto <ggaytan_itcj@hotmail.com>
2024-10-09 12:28:30 -0500

    Avanzando en data collection

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-PROXI_USER = 'US\\GGI2JU'
-PROXI_PWD = 'BoschGil041189+'
-EMAIL = 'gilberto.gaytanpadilla@us.bosch.com'
-SUBJECT = 'Tipo de Cambio'
+PROXI_USER = 'XXXXXXXXXXXXXXXXXXXXXXXXXXX'
+PROXI_PWD = 'XXXXXXXXXXXXXXXXXXXXXXXXX'
+EMAIL = 'XXXXXXXXXXXXXXXXXXXXXXXXXXX'
+SUBJECT = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 PROXIES = {
-    'http': 'http://rb-proxy-na.bosch.com:8080',
-    'https': 'http://rb-proxy-na.bosch.com:8080'
+    'http': 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    'https': 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 }
 HEADERS = {
     'Bmx-Token': '80f8b40305f8c5067445bd8868c2be2fd29f526c597fbadffa7cca63509a25a7',  # Cambia esto por el header que necesites
